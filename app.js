a14552c2b7539c1922ee61d1cf201046aca6ceba
Diogo Munhos <diogo.munhos@sulamerica.com.br>
2018-06-05 11:08:08 -0300

    teste

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require('express');
 const expressGraphQL = require('express-graphql');
 const schema = require('./schema');
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use('/graphql', expressGraphQL({
     schema,
     graphiql: true
 }))
-app.listen(4000, () => {
+app.listen(PORT, () => {
     console.log('Listening...')
 })
